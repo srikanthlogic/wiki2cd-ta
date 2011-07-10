73acee2456435a275ef89305e31618f0b5903e2c
Arunmozhi <aruntheguy@gmail.com>
2011-07-10 22:56:51 +0530

    added a few selectors

--- cleanup.py
+++ cleanup.py
@@ -16,8 +16,8 @@ def cleanup(page):
     #If you want to remove any other section, just add the class or id of the section below with comma seperated
     unwanted_sections_list="""
     div#jump-to-nav, div.top, div#column-one, div#siteNotice, div#purl, div#head,div#footer, div#head-base, div#page-base, div#stub, div#noprint,
-    div#disambig,div.NavFrame,#colophon,.editsection,.toctoggle,.tochidden,.catlinks,.navbox,.sisterproject,.ambox,
-    .toccolours,.topicondiv#f-poweredbyico,div#f-copyrightico,div#featured-star,li#f-viewcount,
+    div#disambig,div.NavFrame,#colophon,.editsection,.toctoggle,.tochidden,.catlinks,.navbox,.sisterproject,.ambox,div#mw-panel,#mw-head,
+    .toccolours,.topicondiv#f-poweredbyico,div#f-copyrightico,div#featured-star,li#f-viewcount,.infobox,#toc,div#p-views,
     li#f-about,li#f-disclaimer,li#f-privacy,.portal
     """
     unwanted_divs = unwanted_sections_list.split(",")
@@ -29,10 +29,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         filename = sys.argv[1]
         inp = codecs.open(filename, encoding="utf-8", mode="r")
-        outp = codecs.open("result.html", encoding="utf-8", mode="w")
-        page = f.read()
+        outp = codecs.open("result.html", encoding="utf-8", mode="w+")
+        page = inp.read()
         result = cleanup(page)
-        outp.write(result)
+        outp.write(result.decode('utf-8'))
         outp.close()
         inp.close()
 
