ba0159694fa080b1c07920112487e2f98de7fb43
Arunmozhi <aruntheguy@gmail.com>
2011-07-11 08:12:50 +0530

    added selectors to the clanup string

--- cleanup.py
+++ cleanup.py
@@ -15,11 +15,8 @@ def cleanup(page):
     document = pq(page)
     #If you want to remove any other section, just add the class or id of the section below with comma seperated
     unwanted_sections_list="""
-    div#jump-to-nav, div.top, div#column-one, div#siteNotice, div#purl, div#head,div#footer, div#head-base, div#page-base, div#stub, div#noprint,
-    div#disambig,div.NavFrame,#colophon,.editsection,.toctoggle,.tochidden,.catlinks,.navbox,.sisterproject,.ambox,div#mw-panel,#mw-head,
-    .toccolours,.topicondiv#f-poweredbyico,div#f-copyrightico,div#featured-star,li#f-viewcount,.infobox,#toc,div#p-views,
-    li#f-about,li#f-disclaimer,li#f-privacy,.portal
-    """
+    .editsection,#mw-panel,script,#mw-head,.infobox,#toc,#jump-to-nav,.reference,
+    .navbox,#footer,#catlinks"""
     unwanted_divs = unwanted_sections_list.split(",")
     for section in unwanted_divs:
         document.remove(section.strip())
